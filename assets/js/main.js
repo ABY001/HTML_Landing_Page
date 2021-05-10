65dc5d43de3444d06bcedd37f76e035aea15b3f1
ABY001 <abiolafadipe88@gmail.com>
2021-05-10 10:02:29 +0100

    header fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -266,3 +266,15 @@ function submitForm() {
   // $(".notification").addClass("is-visible");
   return false;
 }
+
+var header = $(".header");
+
+$(window).scroll(function (e) {
+  if (header.offset().top !== 0) {
+    if (!header.hasClass("shadow")) {
+      header.addClass("shadow");
+    }
+  } else {
+    header.removeClass("shadow");
+  }
+});
